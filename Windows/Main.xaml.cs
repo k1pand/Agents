10ae914c02477259c0066eadf73ef20bacee9194
k1pand <57652556+k1pand@users.noreply.github.com>
2021-05-31 15:53:48 +0500

    1.5
    Добавлен стиль копки

--- Windows/Main.xaml.cs
+++ Windows/Main.xaml.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Agents.Windows
